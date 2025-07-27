b3a1029ca7978c66a4b18e35f12d22e933a20e47
Guruprasath318 <guruprasathkkm@gmail.com>
2025-07-27 22:04:16 +0530

    files

---
+++ casting.py
@@ -0,0 +1,9 @@
+var1=input("Enter a number: ")#string format
+print(var1)
+print(type(var1))
+
+c=int(var1) #integer type
+print(c)
+print(type(c))
+
+print(type(float(c)))
---
+++ vowels.py
@@ -0,0 +1,8 @@
+vowels=["a","e","i","o","u"]
+char=input("Enter a string : ")
+print("The vowels in the string are:")
+for word in char:
+    if word in vowels:
+        print(word)
+    
+
