480b8e05f73922165bde94bab5cc88e764433b95
Guruprasath318 <guruprasathkkm@gmail.com>
2025-11-08 04:14:13 +0530

    Program

---
+++ Interview Preparation Question/Q10.py
@@ -0,0 +1,10 @@
+# percentage calculator
+def pecent(s,d):
+    pecentage = (d/100) * s
+    print(pecentage)
+
+a = int(input("Enter an number: "))
+p = int(input("Enter a percentage: "))
+pecent(a,p)
+
+
